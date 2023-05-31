58b1a4d5170ab0018d11addce1dec368a7ec467e
Prathyush Prashanth Rao <jay.prathyush3615@gmail.com>
2023-05-31 11:42:27 +1000

    updated product info

--- map/producer.py
+++ map/producer.py
@@ -19,9 +19,28 @@ def select_options():
     st.markdown("### Product - Variety List")
 
     # Show vareity
+    unique_key = 0
     if not (option is None):
         for _, row in dt.get_prods_from_licensee(option).iterrows():
-            st.markdown(f"##### Product: {row['Product']}\nVariety: {row['Variety']}\nStores: {row['Store #']} ---")
+            st.markdown("---")
+            cols = st.columns(4)
+            with cols[0]:
+                checked = st.checkbox("Availability", value = row['Available'], key=unique_key)
+                if checked:
+                    select_stores()
+            with cols[1]:
+                st.markdown(f"Product : {row['Product']}")
+            with cols[2]:
+                st.markdown(f"Variety : {row['Variety']}")
+            with cols[3]:
+                if row['Store #'] is None:
+                    st.markdown(f"Stores : None")
+                else:
+                    st.markdown(f"Stores : {row['Store #']}")
+            unique_key += 1
+
+def select_stores():
+    pass
 
 def draw_all():
     title()
